7f3dc0b20003f2ae839a41aacf1b585f55f9a659
Vineeth <vineethchandran5898@gmail.com>
2022-12-27 20:04:54 +0530

    auto refresh while submit problem fixed

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,26 +1,9 @@
-
-
-
-
 var flag = 0;
-function validateform() {
+function validateMail() {
 
-    var name1 = document.getElementById('name').value;
+    
     var emailId = document.getElementById('email').value;
-    var phoneNum = document.getElementById('phone').value;
-    var messageEntered = document.getElementById('message').value;
-
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-    if (name1 == null || name1 == "") {
-        document.getElementById("name-msg").innerHTML = "Name Can't be blank !";
-    } else if (name1.length < 3) {
-        document.getElementById("name-msg").innerHTML = "Name should have 3 letter minimum !";
-    } else {
-        flag = 1
-        document.getElementById("name-msg").innerHTML = "";
-    }
-
     if (emailId == null || emailId == "") {
         document.getElementById("email-msg").innerHTML = "Email can't blank";
     } else if (emailId.match(mailformat)) {
@@ -29,8 +12,23 @@ function validateform() {
     }
     else {
         document.getElementById("email-msg").innerHTML = "Please enter correct email id";
-    }
+    } 
+}
 
+function validateName(){
+    var name1 = document.getElementById('name').value;
+    if (name1 == null || name1 == "") {
+        document.getElementById("name-msg").innerHTML = "Name Can't be blank !";
+    } else if (name1.length < 3) {
+        document.getElementById("name-msg").innerHTML = "Name should have 3 letter minimum !";
+    } else {
+        flag = 1
+        document.getElementById("name-msg").innerHTML = "";
+    }
+}
+
+function validatePhone(){
+    var phoneNum = document.getElementById('phone').value;
     if (phoneNum == null || phoneNum == "") {
         document.getElementById("phone-msg").innerHTML = "phone number can't blank";
     } else if (phoneNum.length < 10) {
@@ -39,7 +37,10 @@ function validateform() {
         flag = 3
         document.getElementById("phone-msg").innerHTML = "";
     }
+}
 
+function validateMessage(){
+    var messageEntered = document.getElementById('message').value;
     if (messageEntered == null || messageEntered == "") {
         document.getElementById("msg-msg").innerHTML = "Message can't blank";
     } else {
@@ -48,7 +49,12 @@ function validateform() {
     }
 }
 
+
 function submitToSheet() {
+    validateName();
+    validateMail();
+    validatePhone();
+    validateMessage();
     if (flag == 4) {
         $("#submit-form").submit((e) => {
             e.preventDefault()
@@ -58,7 +64,7 @@ function submitToSheet() {
                 method: "post",
                 success: function (response) {
                     document.getElementById("submit-button-id").innerHTML = "Message sent successfully", document.getElementById("submit-button-id").style.backgroundColor = 'green'
-                    window.location.reload()
+                    setTimeout(function reloadFunc() { window.location.reload() }, 2000);
                 },
                 error: function (err) {
                     document.getElementById("submit-button-id").innerHTML = "Something Error", document.getElementById("submit-button-id").style.backgroundColor = 'red'
@@ -67,4 +73,12 @@ function submitToSheet() {
             })
         })
     }
+    else
+    {
+        $("#submit-form").submit(function(e) {
+            e.preventDefault();
+        });
+    }
 }
+
+
